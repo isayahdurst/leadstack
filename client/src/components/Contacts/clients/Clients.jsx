116cbc70053f560ea1bdda5fcc272ae8aa878a11
Olga Gavrushenko <ogavby@gmail.com>
2023-03-06 13:29:34 -0800

    fixed unique key prop error and added select client contact state

--- client/src/components/Contacts/clients/Clients.jsx
+++ client/src/components/Contacts/clients/Clients.jsx
@@ -2,11 +2,7 @@ import ClientCard from './ClientCard';
 import { CLIENTS } from './../../../utils/queries';
 import { useQuery } from '@apollo/client';
 
-const Clients = ({ setSelectedClient }) => {
-    const handleClientClick = (client) => {
-        setSelectedClient(client);
-    };
-
+const Clients = ({ selectedClientId, handleClientClick }) => {
     const clientsData = useQuery(CLIENTS);
 
     if (!clientsData.data) {
@@ -16,13 +12,14 @@ const Clients = ({ setSelectedClient }) => {
     return (
         <>
             {clientsData &&
-                clientsData.data.clients.map((contact) => (
+                clientsData.data.clients.map((contact, index) => (
                     <ClientCard
-                        key={contact.id}
+                        key={`$contact.id_${index}`}
                         name={`${contact.first_name} ${contact.last_name}`}
                         phone={contact.phone_number}
                         email={contact.email}
-                        onClick={() => handleClientClick(contact)}
+                        onClick={() => handleClientClick(contact._id, contact)}
+                        selected={selectedClientId === contact._id}
                     />
                 ))}
         </>
